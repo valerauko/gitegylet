372196e55da9c97d8c1fda3fc77b3008b8dfacae
Vale <valerauko@gmail.com>
2022-07-22 16:39:43 +0900

    Async commands have to return Result
    cf https://github.com/tauri-apps/tauri/issues/2533

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -13,23 +13,23 @@ use tauri::State;
 use std::sync::Mutex;
 
 #[tauri::command]
-fn branch_locals(state: State<Mutex<Option<Repository>>>) -> Vec<Branch> {
+async fn branch_locals(state: State<'_, Mutex<Option<Repository>>>) -> Result<Vec<Branch>, String> {
   match &*state.inner().lock().expect("Could not lock mutex") {
-    Some(repo) => Branch::locals(repo),
-    None => vec![]
+    Some(repo) => Ok(Branch::locals(repo)),
+    None => Ok(vec![])
   }
 }
 
 #[tauri::command]
-fn statuses(state: State<Mutex<Option<Repository>>>) -> Vec<Status> {
+async fn statuses(state: State<'_, Mutex<Option<Repository>>>) -> Result<Vec<Status>, String> {
   match &*state.inner().lock().expect("Could not lock mutex") {
-    Some(repo) => Status::all(repo),
-    None => vec![]
+    Some(repo) => Ok(Status::all(repo)),
+    None => Ok(vec![])
   }
 }
 
 #[tauri::command]
-fn open_repo(path: String, state: State<Mutex<Option<Repository>>>) -> Result<String, String> {
+async fn open_repo(path: String, state: State<'_, Mutex<Option<Repository>>>) -> Result<String, String> {
   match Repository::open(&path) {
     Ok(repo) => {
       let mut state = state.lock().expect("Could not lock mutex");
